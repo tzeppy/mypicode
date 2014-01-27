4d010565036a5c2015d7706710bf1f22d89abf90
tzeppy <tzeppy@gmail.com>
2014-01-27 00:32:50 +0000

    print time and delay

--- motionDet/pir_det.py
+++ motionDet/pir_det.py
@@ -2,6 +2,7 @@
 #
 
 import time
+import datetime
 import RPi.GPIO as io
 
 io.setmode(io.BCM)
@@ -12,6 +13,9 @@ io.setup(pir_pin, io.IN)
 
 while True:
     if io.input(pir_pin):
-	print ("PIR Alarm!")
-    time.sleep(0.5)
+	nowstr = datetime.datetime.now().isoformat()
+	print "PIR Alarm at", nowstr
+	time.sleep(30.0)
+    else:
+        time.sleep(0.5)
 
