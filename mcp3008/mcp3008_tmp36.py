d335b9f789c22617407724ec049d5a28ac926d1a
tzeppy <tzeppy@gmail.com>
2014-01-29 20:13:27 -0800

    temp sensor TMP36

---
+++ mcp3008/mcp3008_tmp36.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+import time
+import os
+import RPi.GPIO as GPIO
+
+# read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
+def readadc(adcnum, clockpin, mosipin, misopin, cspin):
+    if ((adcnum > 7) or (adcnum < 0)):
+        return -1
+    GPIO.output(cspin, True)
+    GPIO.output(clockpin, False)  # start clock low
+    GPIO.output(cspin, False)     # bring CS low
+
+    commandout = adcnum
+    commandout |= 0x18  # start bit + single-ended bit
+    commandout <<= 3    # we only need to send 5 bits here
+    for i in range(5):
+        if (commandout & 0x80):
+            GPIO.output(mosipin, True)
+        else:
+            GPIO.output(mosipin, False)
+        commandout <<= 1
+        GPIO.output(clockpin, True)
+        GPIO.output(clockpin, False)
+
+    adcout = 0
+    # read in one empty bit, one null bit and 10 ADC bits
+    for i in range(12):
+        GPIO.output(clockpin, True)
+        GPIO.output(clockpin, False)
+        adcout <<= 1
+        if (GPIO.input(misopin)):
+            adcout |= 0x1
+
+    GPIO.output(cspin, True)
+        
+    adcout >>= 1       # first bit is 'null' so drop it
+    return adcout
+
+# change these as desired - they're the pins connected from the
+# SPI port on the ADC to the Cobbler
+SPICLK = 18
+SPIMISO = 23
+SPIMOSI = 24
+SPICS = 25
+
+GPIO.setmode(GPIO.BCM)
+# set up the SPI interface pins
+GPIO.setup(SPIMOSI, GPIO.OUT)
+GPIO.setup(SPIMISO, GPIO.IN)
+GPIO.setup(SPICLK, GPIO.OUT)
+GPIO.setup(SPICS, GPIO.OUT)
+
+# 10k trim pot connected to adc #0
+potentiometer_adc = 0
+
+try:
+    while True:
+	# read the analog pin
+	trim_pot = readadc(potentiometer_adc, SPICLK, SPIMOSI, SPIMISO, SPICS)
+        voltage = trim_pot * 3.3/1023.0
+        tempC = (voltage-0.5) * 1.0/0.01
+        tempF = tempC *9.0/5.0 + 32.0
+	print "value:%d ,volts:%.2f, tempC:%.2f, tempF:%.2f" % (trim_pot, voltage, tempC, tempF)
+	time.sleep(0.5)
+except KeyboardInterrupt:
+    GPIO.cleanup()
+
+
